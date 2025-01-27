05284800ddb5c0d3055ef15eb6c526cb9dd6b97b
Gokul R <gokulravivcr05@gmail.com>
2025-01-27 23:41:46 +0530

    creating and redering nested sibling html element using react

--- App.js
+++ App.js
@@ -8,11 +8,10 @@
 const headingElement = React.createElement(
   'div',
   { id: 'parent' },
-  React.createElement(
-    'div',
-    { id: 'child' },
+  React.createElement('div', { id: 'child' }, [
     React.createElement('h1', {}, 'Heading 1'),
-  ),
+    React.createElement('h2', {}, 'Heading 2'),
+  ]),
 );
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 console.log(headingElement);
