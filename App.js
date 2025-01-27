c4c8b6e3e651b5ba391cb92e6d4d705e5e4d6cde
Gokul R <gokulravivcr05@gmail.com>
2025-01-27 23:31:13 +0530

    created and rendered nested html element using react

--- App.js
+++ App.js
@@ -1,4 +1,19 @@
-const headingElement=React.createElement("h1",{id:"heading id", xyz:"bla bla"},"Hello World using React!");
-const rootElement=ReactDOM.createRoot(document.getElementById("root"));
+/* <div id="parent">
+<div id="child">
+    <h1>heading 1</h1>
+    <h2>heading 2</h2>
+</div>
+</div> */
+
+const headingElement = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement(
+    'div',
+    { id: 'child' },
+    React.createElement('h1', {}, 'Heading 1'),
+  ),
+);
+const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 console.log(headingElement);
 rootElement.render(headingElement);
