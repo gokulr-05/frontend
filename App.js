4f94ba2afe2a2b6598ab71cb89c4ae17603b97d9
Gokul R <gokulravivcr05@gmail.com>
2025-01-28 01:49:20 +0530

    commit

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
-/* <div id="parent">
-<div id="child">
-    <h1>heading 1</h1>
-    <h2>heading 2</h2>
-</div>
-</div> */
-
-const headingElement = React.createElement(
-  'div',
-  { id: 'parent' },
+const headingElement = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child' }, [
     React.createElement('h1', {}, 'Heading 1'),
     React.createElement('h2', {}, 'Heading 2'),
   ]),
-);
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', {}, 'Heading 1'),
+    React.createElement('h2', {}, 'Heading 2'),
+  ]),
+]);
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 console.log(headingElement);
 rootElement.render(headingElement);
